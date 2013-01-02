73d94278af1a9708fc2d10c3b622807f32bd7075
Jongmin T. Moon <jongmin.t.moon@gmail.com>
2013-01-01 20:57:38 -0800

    MIT License

--- nichi.js
+++ nichi.js
@@ -1,3 +1,14 @@
+/*
+MIT License
+Copyright (c) 2012-2013 Jongmin T. Moon <jongmin.t.moon@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 var nichi = (function(input) {
   var date,
       ms = {
@@ -90,7 +101,7 @@ var nichi = (function(input) {
         }
         else recent = true;
       }
-      if(recent) return 'Just now';
+      if(recent) return 'just now';
       else {
         if(floor > 1) unit += 's';
         if(floor === 1) return 'a ' + unit + ' ago'
